277cdcc47384e37a22adb14d629ff115a5c07668
Niakr1s <pavel2188@gmail.com>
2023-08-27 16:06:30 +0300

    solved 1 two-sum

---
+++ src/pyleetcode/n1_two_sum_test.py
@@ -0,0 +1,14 @@
+import pytest
+from pyleetcode.n1_two_sum import Solution
+
+
+@pytest.mark.parametrize(
+    ("nums", "target", "expected"),
+    (
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ),
+)
+def test(nums, target, expected):
+    assert Solution().twoSum(nums, target) == expected
