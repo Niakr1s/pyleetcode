45848d0b422a283d7f6850817ca9084a7bf53e01
Niakr1s <pavel2188@gmail.com>
2023-09-09 15:49:12 +0300

    n906 start

---
+++ src/pyleetcode/n906_super_palindromes/n906_super_palindromes_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .n906_super_palindromes import Solution
+
+test_params = (
+    ("left", "right", "expected"),
+    (
+        (4, 100, 4),
+        (1, 2, 1),
+    ),
+)
+
+
+@pytest.mark.parametrize(*test_params)
+def test(left, right, expected):
+    assert Solution().superpalindromesInRange(left, right) == expected
