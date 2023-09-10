0eb439cf56fa23e68050c1bce96b65f9e134ffa1
Niakr1s <pavel2188@gmail.com>
2023-09-10 20:54:00 +0300

    simplified n906 c version a lot

--- src/pyleetcode/n906_super_palindromes/n906_super_palindromes.c
+++ src/pyleetcode/n906_super_palindromes/n906_super_palindromes.c
@@ -1,89 +1,48 @@
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-long long pow_ll(long long n, int exp);
-int is_palindrome(char *s);
+int is_palindrome(const char *s);
 void mirror(char *s, int with_middle);
-int str_all(char *s, char ch);
 
-// Input: 1 <= left.length, right.length <= 18
 int superpalindromesInRange(char *left, char *right)
 {
+    const long long l_int, r_int;
+    sscanf(left, "%lld", &l_int);
+    sscanf(right, "%lld", &r_int);
 
-    const long long l_int = atoll(left);
-    const long long r_int = atoll(right);
-
-    // It is our main iterator. On each iteration we will mirror it's string representation
-    // and compute half^2. We should carefully handle cases when half is about to increase
-    // it's length (when 9999 becomes 10000). We are computing first value of half to be
-    // as near as possible to left.
-    // Note: half >= 0 and half.digits <= 5
-    long long half = pow_ll(10, (int)(strlen(left) / 2 / 2)) - 1;
-
-    char s[19];               // main array for storing various string representations
-    sprintf(s, "%lld", half); // we need before loop this to compute with_middle
-    int with_middle = strlen(s) % 2 != 0; // used in mirror function
-    int found = 0; // result of function
-    for (;;)
-    {
-        sprintf(s, "%lld", half);
-        // will len increase at next iteration?
-        const int len_increase = str_all(s, '9');
-        mirror(s, with_middle);
-
-        // n is a palindrome
-        long long n = atoll(s);
-        long long n_pow = n * n;
-
-        // carefully incrementing half for next iteration
-        ++half;
-        if (len_increase)
+    int found = 0;
+    char s[20];
+    for (int with_middle = 0; with_middle <= 1; with_middle++)
+        for (int i = 1; i <= 99999; i++)
         {
-            if (with_middle)
             {
-                half /= 10;
+                if (i >= 9999 && !with_middle)
+                    break;
+
+                sprintf(s, "%d", i);
+                mirror(s, with_middle);
+                if (is_palindrome(s))
+                {
+                    long long n;
+                    sscanf(s, "%lld", &n);
+
+                    n *= n;
+                    if (n < l_int)
+                        continue;
+                    if (n > r_int)
+                        break;
+
+                    sprintf(s, "%lld", n);
+                    if (is_palindrome(s))
+                        found++;
+                }
             }
-            with_middle = !with_middle;
         }
-
-        // various checks
-        if (n_pow < l_int || !is_palindrome(s))
-            continue;
-        if (n_pow > r_int)
-            break;
-
-        sprintf(s, "%lld", n_pow);
-        if (is_palindrome(s))
-        {
-            found++;
-        }
-    }
 
     return found;
 }
 
-int str_all(char *s, char ch)
-{
-    for (int i = 0; i < strlen(s); i++)
-    {
-        if (s[i] != ch)
-            return 0;
-    }
-    return 1;
-}
-
-long long pow_ll(long long n, int exp)
-{
-    long long res = 1;
-    for (int j = 0; j < exp; j++)
-    {
-        res *= n;
-    }
-    return res;
-}
-
-int is_palindrome(char *s)
+int is_palindrome(const char *s)
 {
     int i = 0;
     int j = strlen(s) - 1;
